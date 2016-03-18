2c7486376c5b8c04926ef83d258f08367e93a67a
dscu <denis.casajus@gft.com>
2016-03-18 13:39:46 +0100

    Implements funcionality camara and change view in Options

--- app/src/main/java/com/pp/dcasajus/forkpoint/LocalDetall/ContingutLocal/Photos.java
+++ app/src/main/java/com/pp/dcasajus/forkpoint/LocalDetall/ContingutLocal/Photos.java
@@ -1,6 +1,9 @@
 package com.pp.dcasajus.forkpoint.LocalDetall.ContingutLocal;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -27,7 +30,18 @@ public class Photos extends Fragment {
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
+                if (position == 0) {
+                    if (getActivity().getApplicationContext().getPackageManager().hasSystemFeature(
+                            PackageManager.FEATURE_CAMERA)) {
 
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        // intent.putExtra(MediaStore.EXTRA_OUTPUT,);
+                        startActivityForResult(intent, 100);
+
+                    } else {
+                        Toast.makeText(getActivity().getApplication(), "El teu dispositiu no soporta la camera, estas utilitzant un totxo?", Toast.LENGTH_LONG).show();
+                    }
+            }
             }
         });
         return view;
